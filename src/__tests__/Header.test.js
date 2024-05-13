3f59fad41654d7b69385223dcb6a7fabb91a0ae5
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-05-13 19:02:58 +0300

    phase 2

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom";
+/*import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
@@ -8,4 +8,13 @@ test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
-});
+});*/
+
+
+import React from "react";
+
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
